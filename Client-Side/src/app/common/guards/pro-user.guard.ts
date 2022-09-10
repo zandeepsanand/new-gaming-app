9a5651291536552c4763762e3288323eaefd9e53
Sajan <itssajan@gmail.com>
2022-09-10 07:02:22 +0530

    adding pro user guard function

--- Client-Side/src/app/common/guards/pro-user.guard.ts
+++ Client-Side/src/app/common/guards/pro-user.guard.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { HeroService } from 'src/app/hero.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProUserGuard implements CanActivate {
+  constructor(private heroService: HeroService){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.heroService.isPro();
   }
   
 }
