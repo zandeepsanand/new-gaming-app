a14d40ee8bfc410140343eea8c73148e5e1fce62
Sajan <itssajan@gmail.com>
2022-09-11 09:12:38 +0530

    correcting video

--- Client-Side/src/app/common/components/video-player/video-player.component.ts
+++ Client-Side/src/app/common/components/video-player/video-player.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { environment } from "src/environments/environment";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: "app-video-player",
@@ -8,13 +9,14 @@ import { environment } from "src/environments/environment";
 })
 export class VideoPlayerComponent implements OnInit {
     @Input()
-    src: string;
-    constructor() {}
+    src: any;
+    constructor( private _sanitizer: DomSanitizer) {}
 
     ngOnInit(): void {
-        const url = environment.originUrl;
-        if (this.src.indexOf("?") > -1) {
-            this.src += `origin=${url}`;
-        } else this.src += `&origin=${url}`;
+        // const url = environment.originUrl;
+        // if (this.src.indexOf("?") > -1) {
+        //     this.src += `origin=${url}`;
+        // } else this.src += `&origin=${url}`;
+        this.src = this._sanitizer.bypassSecurityTrustResourceUrl(this.src);
     }
 }
