aa471c099338371a91148f541325193ca30dacdd
Sajan <itssajan@gmail.com>
2022-09-11 08:48:58 +0530

    url correction

--- Client-Side/src/app/common/components/video-player/video-player.component.ts
+++ Client-Side/src/app/common/components/video-player/video-player.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'app-video-player',
-  templateUrl: './video-player.component.html',
-  styleUrls: ['./video-player.component.scss']
+    selector: "app-video-player",
+    templateUrl: "./video-player.component.html",
+    styleUrls: ["./video-player.component.scss"],
 })
 export class VideoPlayerComponent implements OnInit {
-  @Input()
-  src: string;
-  constructor() { }
+    @Input()
+    src: string;
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        const url = environment.originUrl;
+        if (this.src.indexOf("?") > -1) {
+            this.src += `origin=${url}`;
+        } else this.src += `&origin=${url}`;
+    }
 }
